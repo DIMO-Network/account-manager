f3b695444c26297617c33d59e7fea6676299edf0
Bean Jackson <jaggedbytes@protonmail.com>
2025-06-04 10:04:49 -0400

    style: replace husky + lint-staged by lefthook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,70 @@
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tsparser from '@typescript-eslint/parser';
+import antfu from '@antfu/eslint-config';
+import nextPlugin from '@next/eslint-plugin-next';
+import jestDom from 'eslint-plugin-jest-dom';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import playwright from 'eslint-plugin-playwright';
 
-export default [
+export default antfu(
   {
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      parser: tsparser,
-      ecmaVersion: 2020,
-      sourceType: 'module',
+    react: true,
+    typescript: true,
+
+    // Configuration preferences
+    lessOpinionated: true,
+    isInEditor: false,
+
+    // Code style
+    stylistic: {
+      semi: true,
     },
+
+    // Format settings
+    formatters: {
+      css: true,
+    },
+
+    // Ignored paths
+    ignores: [
+      'migrations/**/*',
+    ],
+  },
+  // --- Next.js Specific Rules ---
+  {
     plugins: {
-      '@typescript-eslint': tseslint,
+      '@next/next': nextPlugin,
     },
     rules: {
-      '@typescript-eslint/no-unused-vars': 'warn',
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
     },
   },
+  // --- Accessibility Rules ---
+  jsxA11y.flatConfigs.recommended,
+  // --- Testing Rules ---
   {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-    },
+    files: [
+      '**/*.test.ts?(x)',
+    ],
+    ...jestDom.configs['flat/recommended'],
+  },
+  // --- E2E Testing Rules ---
+  {
+    files: [
+      '**/*.spec.ts',
+      '**/*.e2e.ts',
+    ],
+    ...playwright.configs['flat/recommended'],
+  },
+  // --- Custom Rule Overrides ---
+  {
     rules: {
-      'no-unused-vars': 'warn',
+      'antfu/no-top-level-await': 'off', // Allow top-level await
+      'style/brace-style': ['error', '1tbs'], // Use the default brace style
+      'ts/consistent-type-definitions': ['error', 'type'], // Use `type` instead of `interface`
+      'react/prefer-destructuring-assignment': 'off', // Vscode doesn't support automatically destructuring, it's a pain to add a new variable
+      'node/prefer-global/process': 'off', // Allow using `process.env`
+      'test/padding-around-all': 'error', // Add padding in test files
+      'test/prefer-lowercase-title': 'off', // Allow using uppercase titles in test titles
     },
   },
-];
+);
