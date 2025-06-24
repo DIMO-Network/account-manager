a0516fdda0ce4b11420b4733d1f7f81f976077d2
Bean Jackson <jaggedbytes@protonmail.com>
2025-06-24 16:41:55 -0400

    fix(migration): handle existing data in 0004_marvelous_shadowcat

--- migrations/0004_marvelous_shadowcat.sql
+++ migrations/0004_marvelous_shadowcat.sql
@@ -1,2 +1,4 @@
-ALTER TABLE "device_subscriptions" ADD COLUMN "connection_id" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "device_subscriptions" ADD COLUMN "connection_id" text;--> statement-breakpoint
+UPDATE "device_subscriptions" SET "connection_id" = "serial_number" WHERE "connection_id" IS NULL;--> statement-breakpoint
+ALTER TABLE "device_subscriptions" ALTER COLUMN "connection_id" SET NOT NULL;--> statement-breakpoint
 ALTER TABLE "device_subscriptions" ADD CONSTRAINT "device_subscriptions_connection_id_unique" UNIQUE("connection_id");
