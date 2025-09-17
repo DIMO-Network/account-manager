da56b1a9cf551f4508a302ebda285196b5b8fb82
Bean Jackson <96313721+jaggedbytes@users.noreply.github.com>
2025-09-17 13:07:41 -0400

    style: fix import sorting and indentation issues (#74)
    * style: fix import sorting and indentation issues
    
    * fix: update version mismatch with ESLint configuration
    
    * style: fix import sorting and indentation issues again

--- src/libs/Stripe.ts
+++ src/libs/Stripe.ts
@@ -11,7 +11,7 @@ export const stripe = (): Stripe => {
     }
 
     stripeInstance = new Stripe(secretKey, {
-      apiVersion: '2025-06-30.basil',
+      apiVersion: '2025-08-27.basil',
     });
   }
 
