997be8f070539345525139cf03f90ff2b9327b89
Bean Jackson <jaggedbytes@protonmail.com>
2026-06-22 15:49:00 -0400

    fix: remove server-only Spotlight import

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -2,11 +2,13 @@
 // The added config here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 import * as Sentry from '@sentry/nextjs';
-import * as Spotlight from '@spotlightjs/spotlight';
 
 if (!process.env.NEXT_PUBLIC_SENTRY_DISABLED) {
   Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+
+    // Enable Spotlight in development
+    spotlight: process.env.NODE_ENV === 'development',
 
     // Add optional integrations for additional features
     integrations: [
@@ -30,10 +32,6 @@ if (!process.env.NEXT_PUBLIC_SENTRY_DISABLED) {
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false,
   });
-
-  if (process.env.NODE_ENV === 'development') {
-    Spotlight.init();
-  }
 }
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
